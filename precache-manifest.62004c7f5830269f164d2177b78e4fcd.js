5f70e98fab5f34a49ee4ccdd4faeee1614005f9d
willisIT <williamskyei24@gmail.com>
2019-06-07 17:45:09 +0100

    initial commit

---
+++ precache-manifest.62004c7f5830269f164d2177b78e4fcd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b8c16623d7cc957ec59f23f668c03b4",
+    "url": "/img/lead2.4b8c1662.jpg"
+  },
+  {
+    "revision": "e762358b52f39ebdf94ba4ef7a2a945a",
+    "url": "/vision.jpg"
+  },
+  {
+    "revision": "659056776b531c421444f40872dbabfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e19b201e390fe940e67",
+    "url": "/css/chunk-vendors.469fa827.css"
+  },
+  {
+    "revision": "f7bec7ae5745de7f3d5efebe60ec45e4",
+    "url": "/img/picskids.f7bec7ae.jpg"
+  },
+  {
+    "revision": "0f298da6c83b8b474e59f55aee493f17",
+    "url": "/img/grouppic1.0f298da6.jpg"
+  },
+  {
+    "revision": "db33591ff93c22a6c7b9995def8c8da5",
+    "url": "/img/lead1.db33591f.jpg"
+  },
+  {
+    "revision": "fd01de480f65d725eca2774e8df9d24b",
+    "url": "/img/about.fd01de48.jpg"
+  },
+  {
+    "revision": "4937c01cec98c6ed8a14edf99c0dbccd",
+    "url": "/img/logo.4937c01c.jpg"
+  },
+  {
+    "revision": "e74614df601a76b26095",
+    "url": "/js/app.98a370b9.js"
+  },
+  {
+    "revision": "5e19b201e390fe940e67",
+    "url": "/js/chunk-vendors.d2651d29.js"
+  },
+  {
+    "revision": "bf3f9740232411365c6ab4caa68bf3f2",
+    "url": "/passion.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e130aef20e2e35148d80b1d514e4ecef",
+    "url": "/group.jpg"
+  },
+  {
+    "revision": "7176a9ac2f8491d1ea1ee0b0c22fcd68",
+    "url": "/john1.jpg"
+  },
+  {
+    "revision": "2bcbeff5ec79c9ed96680e772b2f5670",
+    "url": "/profoduru.jpg"
+  },
+  {
+    "revision": "e74614df601a76b26095",
+    "url": "/css/app.d975cdae.css"
+  },
+  {
+    "revision": "a9b298718012a28fa8544472083acaf6",
+    "url": "/asare.jpg"
+  }
+];
